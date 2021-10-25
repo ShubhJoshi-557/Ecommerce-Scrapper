b2dd7587e9256b8f5d411aa9295484643e8cebe2
Shubh Joshi <shubhjoshi80@gmail.com>
2021-10-26 02:03:30 +0530

    add main.js

---
+++ main.js
@@ -0,0 +1,94 @@
+// npm init --yes
+// npm install axios
+// npm install minimist
+// npm install jsdom
+
+const express = require("express")
+const minimist = require("minimist")
+const axios = require("axios")
+const bodyParser = require("body-parser")
+let jsdom = require("jsdom");
+const path = require('path');
+let ejs = require('ejs')
+
+const app = express();
+
+app.set("views", path.join(__dirname,"views"));
+app.set('view engine','ejs');
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/',function(req,res){
+    res.sendFile('index.html',{root: __dirname+'/public'});
+});
+
+app.post('/search',(req,response) => {
+    console.log(req.body.search);
+    var flipkart_data;
+    var amazon_data;
+    axios.get('https://www.flipkart.com/search?q='+req.body.search).then((res) =>{
+        let html = res.data;
+        let dom = new jsdom.JSDOM(html);
+        let document = dom.window.document;
+        let maindiv = document.querySelectorAll('[class="_1YokD2 _3Mn1Gg"]')
+        let product = maindiv[1].querySelectorAll('[class="_1AtVbE col-12-12"]')
+        let divType = product[0].querySelectorAll('[data-id]');
+        console.log(divType.length);
+        let productName = [];
+        let productPrice = [];
+        let productRating = [];
+        let productImg = [];
+        let productLink = [];
+        let finalProduct
+        if(divType.length==1){
+            finalProduct = product;
+            for (let i = 2; i < 5; i++) {
+                productName.push(finalProduct[i].querySelector('[class="_4rR01T"]').textContent);
+                productPrice.push(finalProduct[i].querySelector('[class="_30jeq3 _1_WHN1"]').textContent);
+                productRating.push(finalProduct[i].querySelector('[class="_3LWZlK"]').textContent);
+                productImg.push(finalProduct[i].querySelector('img').src);
+                productLink.push("https://www.flipkart.com"+finalProduct[i].querySelector('a').href);
+            }
+        }else{
+            finalProduct = divType;
+            for (let i = 0; i < 3; i++) {
+                productName.push(finalProduct[i].querySelector('[class="s1Q9rs"]').textContent);
+                productPrice.push(finalProduct[i].querySelector('[class="_30jeq3"]').textContent);
+                productRating.push(finalProduct[i].querySelector('[class="_3LWZlK"]').textContent);
+                productImg.push(finalProduct[i].querySelector('img').src);
+                productLink.push("https://www.flipkart.com"+finalProduct[i].querySelector('a').href);
+            }
+        }
+        flipkart_data = [productName,productPrice,productRating,productImg,productLink]
+    });
+    delay(9000).then(axios.get('https://www.amazon.in/s?k='+req.body.search).then((res) =>{
+        let html = res.data;
+        let dom = new jsdom.JSDOM(html);
+        let document = dom.window.document;
+        let product = document.querySelectorAll('[data-index]')
+        let am_productName = [];
+        let am_productPrice = [];
+        let am_productRating = [];
+        let am_productImg = [];
+        let am_productLink = [];
+        for (let i = 2; i < 5; i++) {
+            am_productName.push(product[i].querySelector('h2 span').textContent);
+            am_productPrice.push(product[i].querySelector('i span').textContent);
+            am_productRating.push(product[i].querySelector('[class="a-price-whole"]').textContent);
+            am_productImg.push(product[i].querySelector('span a div img').src);
+            am_productLink.push("www.amazon.in"+product[i].querySelector('h2 a').href);
+        }   
+        amazon_data = [am_productName,am_productPrice,am_productRating,am_productImg,am_productLink] 
+        console.log(amazon_data);
+        console.log(flipkart_data);
+        response.render('index.ejs',{root: __dirname+'/views', tite: 'Search Results', amazon_data, flipkart_data});
+    }));
+});
+
+
+let args = minimist(process.argv);
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
