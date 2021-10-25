642af3ba43ae0ad1facdbd2f2e81e4a3be00d7a5
Shubh Joshi <shubhjoshi80@gmail.com>
2021-10-26 02:04:23 +0530

    add public folder

---
+++ public/index.js
@@ -0,0 +1,16 @@
+
+for(i=0; i<3;i++) {
+    var board = document.createElement('div');
+    board.className = "card";
+    board.innerHTML ="<div class='cover'><img src='https://rukminim1.flixcart.com/image/612/612/ku5ufm80/antiseptic/o/r/x/disinfectant-lime-fresh-dettol-original-imag7caxyfvgubtm.jpeg?q=70' alt='cover' style='max-width:230px;max-height:230px;width: auto;height: auto; margin: auto;'/></div><div class='card-content'><h4><a href='#'>New Apple iPhone 12 Pro123</a></h4><p class='Price'>Dido</p></div>"
+    document.getElementById('amazon').appendChild(board);
+    console.log("brrrrr");
+}
+
+for(i=0; i<3;i++) {
+    var board = document.createElement('div');
+    board.className = "card";
+    board.innerHTML ="<div class='cover'><img src='https://rukminim1.flixcart.com/image/612/612/ku5ufm80/antiseptic/o/r/x/disinfectant-lime-fresh-dettol-original-imag7caxyfvgubtm.jpeg?q=70' alt='cover' style='max-width:230px;max-height:230px;width: auto;height: auto; margin: auto;'/></div><div class='card-content'><h4><a href='#'>New Apple iPhone 12 Pro123</a></h4><p class='Price'>Dido</p></div>"
+    document.getElementById('flipkart').appendChild(board);
+    console.log("brrrrr");
+}
